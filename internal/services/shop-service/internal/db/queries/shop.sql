f8db35e82a386ebf0c8e4076a6725a2fbf63f9c8
tojinguyen <toainguyenprogramminggame@gmail.com>
2025-07-05 12:21:59 +0700

    feat: implement create shop query

---
+++ internal/services/shop-service/internal/db/queries/shop.sql
@@ -0,0 +1,18 @@
+-- name: CreateShop :one
+INSERT INTO shops (
+  id,
+  owner_id,
+  shop_name,
+  avatar_url,
+  banner_url,
+  shop_description,
+  address_id,
+  phone,
+  email,
+  rating,
+  active_at,
+  created_at,
+  updated_at
+) VALUES (
+  $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
+) RETURNING *;
