5c70eb96275800a65da3207b5e4822cd02dc907b
tojinguyen <toainguyenprogramminggame@gmail.com>
2025-07-05 11:34:14 +0700

    feat: create addresses table migration

--- internal/services/shop-service/internal/db/migrations/00004_create_shop_addresses_table.sql
+++ internal/services/shop-service/internal/db/migrations/00004_create_shop_addresses_table.sql
@@ -1,9 +1,25 @@
 -- +goose Up
 -- +goose StatementBegin
-SELECT 'up SQL query';
+CREATE TABLE IF NOT EXISTS addresses (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    shop_id UUID NOT NULL,
+    street VARCHAR(255) NOT NULL,
+    ward VARCHAR(100),
+    district VARCHAR(100),
+    city VARCHAR(100),
+    country VARCHAR(100) DEFAULT 'Vietnam',
+    lat DOUBLE PRECISION,
+    long DOUBLE PRECISION,
+    deleted_at TIMESTAMP WITH TIME ZONE,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
+
+    CONSTRAINT fk_address_shop FOREIGN KEY (shop_id) REFERENCES shops(id) ON DELETE CASCADE
+);
+
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-SELECT 'down SQL query';
+DROP TABLE IF EXISTS addresses;
 -- +goose StatementEnd
