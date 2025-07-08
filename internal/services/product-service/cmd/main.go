bc307a2dedee2de48fa3bf3dbb4068676414fdb5
tojinguyen <toainguyenprogramminggame@gmail.com>
2025-07-08 23:56:11 +0700

    update: prevent shutdown app instantly open

--- internal/services/product-service/cmd/main.go
+++ internal/services/product-service/cmd/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/toji-dev/go-shop/internal/services/product-service/internal/config"
@@ -30,7 +35,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to initialize dependency container: %v", err)
 	}
-	dependencyContainer.Close()
+	defer dependencyContainer.Close()
 
 	router := router.SetupRoutes(r, dependencyContainer)
 
@@ -50,4 +55,19 @@ func main() {
 			log.Fatalf("Failed to start server: %v", err)
 		}
 	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	// Give outstanding requests a deadline of 30 seconds to complete
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
+	log.Println("Server exited")
 }
