5b8c8f43878ab3066faaef9d1198e3df6643b7ef
toji-dev <toainguyenprogramminggame@gmail.com>
2025-07-10 00:21:19 +0700

    update: remove not use route

--- internal/services/product-service/internal/router/router.go
+++ internal/services/product-service/internal/router/router.go
@@ -3,26 +3,15 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	dependency_container "github.com/toji-dev/go-shop/internal/services/product-service/internal/dependency-container"
-	"github.com/toji-dev/go-shop/internal/services/product-service/internal/handler"
 )
 
 func SetupRoutes(r *gin.Engine, serviceContainer *dependency_container.DependencyContainer) *gin.Engine {
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
 
-	productHandler := handler.NewProductHandler(serviceContainer.GetProductRepository(), serviceContainer.GetRedis())
-
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, "pong")
 	})
-
-	v1 := r.Group("api/v1")
-	{
-		products := v1.Group("/products")
-		{
-			products.POST("", productHandler.CreateProduct)
-		}
-	}
 
 	return r
 }
