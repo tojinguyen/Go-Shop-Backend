4cb775e594207f9da40b1ee77ce5c1ea1a52a54d
toaidev <164828669+toaidev@users.noreply.github.com>
2025-07-11 18:05:38 +0700

    feat: implement product repo

--- internal/services/product-service/internal/repository/product_repo_impl.go
+++ internal/services/product-service/internal/repository/product_repo_impl.go
@@ -2,7 +2,9 @@ package repository
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/toji-dev/go-shop/internal/pkg/converter"
 	postgresql_infra "github.com/toji-dev/go-shop/internal/pkg/infra/postgreql-infra"
 	"github.com/toji-dev/go-shop/internal/services/product-service/internal/db/sqlc"
 	domain "github.com/toji-dev/go-shop/internal/services/product-service/internal/domain/product"
@@ -26,12 +28,47 @@ func NewProductRepository(db *postgresql_infra.PostgreSQLService) ProductReposit
 }
 
 func (r *pgProductRepository) Save(ctx context.Context, product *domain.Product) error {
+	params := sqlc.CreateProductParams{
+		ShopID:             converter.UUIDToPgUUID(product.ShopID()),
+		ProductName:        product.Name(),
+		ThumbnailUrl:       converter.StringToPgText(product.ThumbnailURL()),
+		ProductDescription: converter.StringToPgText(product.Description()),
+		CategoryID:         converter.UUIDToPgUUID(product.CategoryID()),
+		Price:              converter.Float64ToPgNumeric(product.Price().Amount),
+		Quantity:           int32(product.Quantity()),
+		ReserveQuantity:    int32(product.Quantity()),
+		ProductStatus:      sqlc.ProductStatus(product.Status()),
+	}
+
+	_, err := r.queries.CreateProduct(ctx, params)
+	if err != nil {
+		return fmt.Errorf("failed to save product: %w", err)
+	}
 
 	return nil
 }
 
 func (r *pgProductRepository) GetByID(ctx context.Context, id string) (*domain.Product, error) {
-	return nil, nil
+	product, err := r.queries.GetProductByID(ctx, converter.StringToPgUUID(id))
+	if err != nil {
+		return nil, fmt.Errorf("failed to get product by ID: %w", err)
+	}
+
+	productDomain, err := domain.NewProduct(
+		product.ShopID.String(),
+		product.ProductName,
+		product.ThumbnailUrl.String,
+		product.ProductDescription.String,
+		converter.PgUUIDToUUID(product.CategoryID),
+		domain.Price{Amount: *converter.PgNumericToFloat64Ptr(product.Price)},
+		int(product.Quantity),
+	)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to convert product to domain: %w", err)
+	}
+
+	return productDomain, nil
 }
 
 func (r *pgProductRepository) GetByShopID(ctx context.Context, shopID string) ([]*domain.Product, error) {
