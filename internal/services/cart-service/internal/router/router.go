f30f8203dc2a93f5dd174545bac546fe43a47d58
toaidev <164828669+toaidev@users.noreply.github.com>
2025-07-15 11:19:38 +0700

    feat: add endpoint health check for cart

--- internal/services/cart-service/internal/router/router.go
+++ internal/services/cart-service/internal/router/router.go
@@ -1,10 +1,17 @@
 package router
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	dependency_container "github.com/toji-dev/go-shop/internal/services/cart-service/internal/dependency-container"
 )
 
 func SetupRoutes(r *gin.Engine, dependencyContainer *dependency_container.DependencyContainer) {
-
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status":  "ok",
+			"service": "cart-service",
+		})
+	})
 }
