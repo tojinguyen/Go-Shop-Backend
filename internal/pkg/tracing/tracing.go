8dece363266dd214eafe269fd6312060f79907cb
toji-dev <toainguyenprogramminggame@gmail.com>
2025-08-19 00:53:28 +0700

    feat: remove prometheus, grafana

--- internal/pkg/tracing/tracing.go
+++ internal/pkg/tracing/tracing.go
@@ -10,7 +10,7 @@ import (
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.24.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.34.0"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
